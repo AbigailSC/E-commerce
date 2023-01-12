7862ebecd73a498d143996ef1645f1a61ebc3f4f
AbigailSc <abigailsarzuri@gmail.com>
2023-01-12 20:15:19 -0300

    feat: connection to mongobd

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import indexRoute from '@routes/index.routes';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
@@ -7,6 +11,13 @@ app.get('/', (_req, res) => {
   res.json({ message: 'Hello world' });
 });
 
+const mongo = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongo as string)
+  .then(() => console.log('✔️ ...Connected to mongoDB Atlas'))
+  .catch((error) => console.error(error));
+
 app.use('/', indexRoute);
-// a
+
 app.listen(3000, () => console.log('✔️ ...Server running on port 3000'));
