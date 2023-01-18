e954b59fe63c80617d822072bdf37154ed939fc3
Ivan Ignacio Peña Mattos <76266733+IP-Mattos@users.noreply.github.com>
2023-01-18 18:11:37 -0300

    feat: get user by id

--- src/routes/User/user.routes.ts
+++ src/routes/User/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createUser, getUsers } from '@controllers/user';
+import { createUser, getUsers, getUserById } from '@controllers/user';
 
 const router: Router = Router();
 
 router.post('/', createUser);
 router.get('/', getUsers);
+router.get('/:id_user', getUserById);
 
 export default router;
