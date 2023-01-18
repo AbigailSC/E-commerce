907efaa1c8708cbe39da407a2e2c0e4982f50a0e
Ivan Ignacio Peña Mattos <76266733+IP-Mattos@users.noreply.github.com>
2023-01-18 18:39:05 -0300

    feat: create delete user route

--- src/routes/User/user.routes.ts
+++ src/routes/User/user.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { createUser, getUsers, getUserById } from '@controllers/user';
+import {
+  createUser,
+  getUsers,
+  getUserById,
+  deleteUser
+} from '@controllers/user';
 
 const router: Router = Router();
 
 router.post('/', createUser);
 router.get('/', getUsers);
 router.get('/:idUser', getUserById);
+router.delete('/:idUser', deleteUser);
 
 export default router;
