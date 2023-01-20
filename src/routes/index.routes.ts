be995a5c8bcbf1802ecfb08cb09bba73fd9b84f0
AbigailSc <abigailsarzuri@gmail.com>
2023-01-20 01:19:45 -0300

    feat: create and view category

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { products } from './Products';
 import { users } from './Users';
+import { category } from './Category';
 
 const router = Router();
 
 router.use('/users', users);
 router.use('/products', products);
+router.use('/category', category);
 
 export default router;
